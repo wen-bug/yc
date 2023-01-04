ee934297fdb6a8a1bf6c1f4ef6ee02cb180a3eb9
温华 <1097641954@qq.com>
2023-01-04 20:54:46 +0800

    add card

---
+++ src/components/animation/card.vue
@@ -0,0 +1,38 @@
+<script setup lang='ts'>
+// 身份卡片
+</script>
+
+<template>
+    <div>
+        <div class="card">
+            <p class="name">定风波</p>
+            <p class="tips">华友拉萨扩大解放撒罗尼法拉盛</p>
+        </div>
+    </div>
+</template>
+
+<style lang='less' scoped>
+.card{
+    background-image: url(../../assets/author.jpg);
+    background-size: cover;
+    width: 100px;
+    height: 100px;
+    overflow: hidden;
+    .name{
+        position: relative;
+        left: -10em;
+        transition: 1s;
+    }
+    .tips{
+        position: relative;
+        bottom: -5em;
+        transition: 1s;
+    }
+}
+.card:hover .name {
+    left: 0;
+}
+.card:hover .tips{
+    bottom: 0;
+}
+</style>
---
+++ src/components/animation/card2.vue
@@ -0,0 +1,58 @@
+<script setup lang='ts'>
+// #旋转双面卡片
+</script>
+
+<template>
+    <div class="box">
+        <div class="card">
+            <div class="front">
+                <p>
+                    我是正面
+                </p>
+            </div>
+            <div class="back">
+                <p> 我是反面</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang='less' scoped>
+.card {
+    width: 100px;
+    height: 200px;
+    justify-content: center;
+    box-shadow: 0 0 10px rgb(192, 192, 192);
+    position: relative;
+    transition: 1s ease-in-out;
+
+    .front {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        transition: 1s ease-in-out;
+    }
+
+    .back {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        transform: rotateY(180deg);
+        transition: 1s ease-in-out;
+        opacity: 0;
+    }
+}
+
+.card:hover .front {
+    transform: rotateY(180deg);
+    opacity: 0;
+
+}
+
+.card:hover .back {
+    transform: rotateY(0deg);
+    opacity: 1;
+    background-color: rgb(110, 86, 54);
+
+}
+</style>
