25e8dd3192293e9e1d968d4acbe530197f3373d3
定风波 <1097641954@qq.com>
2023-01-10 17:30:45 +0800

    css test

---
+++ src/components/animation/tocss.vue
@@ -0,0 +1,28 @@
+<script setup lang='ts'>
+const m = () => {
+    let dom = document.getElementsByClassName('m')[0] as HTMLElement
+    let styles = getComputedStyle(dom)
+    // let color = styles.getPropertyValue('--color')
+    styles.setProperty('--color','#999')
+    
+    console.log(dom.style.getPropertyValue('--color'));
+
+}   
+</script>
+
+<template>
+    <div class="m">
+        <p>sfwfwef</p>
+        <button @click="m">dd</button>
+    </div>
+</template>
+
+<style lang='less' scoped>
+.m {
+    --color: #fff;
+}
+
+p {
+    color: var(--color);
+}
+</style>
